86308438580cea9b620ee07f9e2f9ec04f6bf16f
Mateusz <kieszkowski.mateusz@gmail.com>
2018-02-09 21:37:39 +0100

    MKzapytania

---
+++ kret_sql/Mateusz_Kieszkowski_zapytania.sql
@@ -0,0 +1,17 @@
+﻿SELECT d.id_dostawcy , dc.id_czesci
+FROM dostawcy_czesci as dc, dostawcy as d
+WHERE dc.id_dostawcy = d.id_dostawcy
+AND id_czesci IS NULL
+
+SELECT id_dostawcy, COUNT(id_czesci)
+FROM dostawcy_czesci
+GROUP BY id_dostawcy
+ORDER BY COUNT(id_czesci) DESC
+
+SELECT d.id_dostawcy
+FROM dostawcy d, dostawcy_czesci dc
+WHERE d.id_dostawcy = dc.id_dostawcy
+AND data_zam IS NULL
+
+SELECT AVG(data_przyj_mag - data_zam) AS avg_time
+FROM dostawcy_czesci
