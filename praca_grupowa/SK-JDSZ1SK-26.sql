aa910223f06f30fa20985eea3a0893f89e7f013d
Mateusz <kieszkowski.mateusz@gmail.com>
2018-02-25 12:51:40 +0100

    JDSZ1SK-26 liczba wnioskow w zaleznosci od kanalu

---
+++ praca_grupowa/SK-JDSZ1SK-26.sql
@@ -0,0 +1,6 @@
+﻿SELECT to_char(data_utworzenia,'YYYY-MM'),kanal, count(1),
+  sum(count(1)) OVER (PARTITION BY to_char(data_utworzenia,'YYYY-MM')),
+  ROUND(count(1) / sum(count(1)) OVER (PARTITION BY to_char(data_utworzenia,'YYYY-MM'))::NUMERIC * 100,3) procent_z_sumy
+FROM wnioski
+GROUP BY 1,2
+ORDER BY 1,2
