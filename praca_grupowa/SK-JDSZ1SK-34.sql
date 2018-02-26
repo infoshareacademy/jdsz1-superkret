4a3bc5423cc122eecc4c0cb80a4a484fd9b1b910
grzegorzskipper <grzegorz.szyperek@gmail.com>
2018-02-26 21:31:11 +0100

    Liczba wniosków w zależności od dnia tygodnia i kanału - po Testach gotowy do sprawdzenia i robienia prezentacji

--- praca_grupowa/SK-JDSZ1SK-34.sql
+++ praca_grupowa/SK-JDSZ1SK-34.sql
@@ -1,17 +1,26 @@
 -- SK-JDSZ1SK-34
 --Liczba wniosków w zależności od dnia tygodnia i kanału
 
-
-SELECT
-  to_char(data_utworzenia, 'day') dzien, kanal,
-  count(id) suma_wnioskow,
-  sum(count(id)) over(partition by kanal) okno_kanal,
-  round(count(id)/sum(count(id)) over(partition by kanal)::numeric,4) proc_kanal, 
--- udział poszczególnych dni tygodnia w kanale (bezpośredni lub pośredni)
-  sum(count(id)) over(partition by to_char(data_utworzenia, 'day')) okno_data,
-  round(count(id)/sum(count(id)) over(partition by to_char(data_utworzenia, 'day'))::numeric,4) proc_data 
--- udzial poszczegolnych kanalow w konkretnym dniu tygodnia
-  --sum(count(id)) over()
-FROM wnioski
-GROUP BY 1, 2;
+with moje_dane as ( --Dodaję WITH by posortować dane
+    SELECT
+      to_char(data_utworzenia, 'D'),
+      to_char(data_utworzenia, 'day') dzien,
+      kanal,
+      count(id) suma_wnioskow,
+      sum(count(id))
+      OVER (
+        PARTITION BY kanal ) okno_kanal,
+      round(count(id) / sum(count(id))
+      OVER (
+        PARTITION BY kanal ) :: NUMERIC, 4)  proc_kanal, -- udział poszczególnych dni tygodnia w kanale (bezpośredni lub pośredni)
+      sum(count(id)) OVER (PARTITION BY to_char(data_utworzenia, 'day') ) okno_data,
+      round(count(id) / sum(count(id)) OVER (PARTITION BY to_char(data_utworzenia, 'day') ) :: NUMERIC, 4) proc_data -- udzial poszczegolnych kanalow w konkretnym dniu tygodnia
+    --wykasowałem wykomentowane linijki
+    FROM wnioski
+    GROUP BY 1,2, 3
+    --dodaję sortowanie bo dane są nieuporządkowane
+    ORDER BY 1
+)
+SELECT dzien, suma_wnioskow, okno_kanal, proc_kanal, okno_data, proc_data  --selekcjonuję dane które były w WITH ale już uporządkowane
+FROM moje_dane;
 
