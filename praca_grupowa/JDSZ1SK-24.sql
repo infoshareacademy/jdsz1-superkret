7c261aa9fc1422b60d92da9afa7fae8c703e9d73
Rzonkil <dopmarta@gmail.com>
2018-02-28 19:14:50 +0100

    dodałam kolejny etap od analizy_operatora do analizy_prawnej

--- praca_grupowa/JDSZ1SK-24.sql
+++ praca_grupowa/JDSZ1SK-24.sql
@@ -9,6 +9,10 @@ przykład:
 - akceptacja 40% (z wszystkich które przeszły z poprzedniego etapu), 32% (z wszystkich)
 - odrzucenie 50% (z wszystkich które przeszły z poprzedniego etapu), 40% (z wszystkich)
 - drop off 10%  (z wszystkich które przeszły z poprzedniego etapu), 8% (z wszystkich)
+
+4) odpowiedź po analizie prawnej:
+założenia jw
+
 */
 
 
@@ -23,9 +27,17 @@ select
 
   --podpunkt 3)
   count(case when status_odp = 'zaakceptowany' then o.id end)/count(a.id)::numeric proc_zaakceptowanych_odp_operator, -- procent zaakceptowanych z odpowiedzi operatora
-  count(case when status_odp LIKE 'odrzuco%' then o.id end)/count(a.id)::numeric proc_odrzuconych_odp_operator, -- procent odrzuconych z odpowiedzi operatora
+  count(case when status_
+odp LIKE 'odrzuco%' then o.id end)/count(a.id)::numeric proc_odrzuconych_odp_operator, -- procent odrzuconych z odpowiedzi operatora
   (count(a.id)-count(o.id))/count(a.id)::numeric proc_drop_off_odp_operator  --dropp off czyli procent króty nie przeszedł z analizy
+
+-- podpunkt 4)
+  count(case when ap.status = 'zaakceptowany' then ap.id end)/count(o.id)::numeric proc_zaakceptowanych_analiza_sadowa, -- procent zaakceptowanych po analizie prawnej
+  count(case when ap.status LIKE 'odrzucony' then ap.id end)/count(o.id)::numeric proc_odrzuconych_analiza_sadowa, -- procent odrzuconych po analizie
+  (count(o.id)-count(ap.id))/count(o.id)::numeric proc_drop_off_odp_operator  --dropp off czyli procent króry ma inny status odpowiedzi
+
 from wnioski w
 left join analizy_wnioskow a ON w.id = a.id_wniosku  --łaczenie tabeli z analizą by wyłuskać pierwszy funnel
-LEFT JOIN analiza_operatora o on w.id = o.id_wniosku; --łaczenie tabeli z odpowiedzią operatora by wyliczyć drugi funnel
+LEFT JOIN analiza_operatora o on w.id = o.id_wniosku --łaczenie tabeli z odpowiedzią operatora by wyliczyć drugi funnel
+left join analiza_prawna ap on w.id=ap.id_wniosku; -- trzeci funnel
 
