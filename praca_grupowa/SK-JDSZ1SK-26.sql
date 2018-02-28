b1ed96031e1c047c043d40e3218629ae53be559d
Rzonkil <dopmarta@gmail.com>
2018-02-28 21:12:43 +0100

    dodane  aliasy i zależności od kanału (okno_kanalu i procent_kanalu)

--- praca_grupowa/SK-JDSZ1SK-26.sql
+++ praca_grupowa/SK-JDSZ1SK-26.sql
@@ -1,6 +1,16 @@
-﻿SELECT to_char(data_utworzenia,'YYYY-MM'),kanal, count(1),
-  sum(count(1)) OVER (PARTITION BY to_char(data_utworzenia,'YYYY-MM')),
-  ROUND(count(1) / sum(count(1)) OVER (PARTITION BY to_char(data_utworzenia,'YYYY-MM'))::NUMERIC * 100,3) procent_z_sumy
+-- liczba wniosków w zależności od kanału i czasu
+
+
+SELECT to_char(data_utworzenia,'YYYY-MM') as data, kanal,
+  count(id) suma_wnioskow,
+ sum(count(id)) OVER (PARTITION BY to_char(data_utworzenia,'YYYY-MM')) okno_daty,
+ROUND(count(id) / sum(count(1)) OVER (PARTITION BY to_char(data_utworzenia,'YYYY-MM'))::NUMERIC * 100,3) procent_daty,
+ sum(count(id)) OVER (PARTITION BY kanal) okno_kanalu,
+ROUND(count(id) / sum(count(1)) OVER (PARTITION BY kanal)::NUMERIC * 100,3) procent_kanalu
+
 FROM wnioski
-GROUP BY 1,2
-ORDER BY 1,2
+GROUP BY 2,1
+ORDER BY 1,2;
+
+--dodałam aliasy i zależności od kanału (okno_kanalu i procent_kanalu)
+
