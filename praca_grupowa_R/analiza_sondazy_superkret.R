c0d3ff01743c5e71b564239d954ee04dce04d7d2
grzegorzskipper <grzegorz.szyperek@gmail.com>
2018-03-25 12:50:50 +0200

    JDSZ1SK-49 Project R 1 - election polls SLD -JDSZ1SK-54

--- praca_grupowa_R/analiza_sondazy_superkret.R
+++ praca_grupowa_R/analiza_sondazy_superkret.R
@@ -5,7 +5,7 @@ active_packages <- function(pkg){
   sapply(pkg, require, character.only = TRUE)
   print("packages ready")
 }
-packages <- c("devtools", "openxlsx", "RPostgreSQL", "dplyr", "XML", "RCurl")
+packages <- c("devtools", "openxlsx", "RPostgreSQL", "dplyr", "XML", "RCurl", "rvest", "lubridate", "tidyverse", "scales")
 
 
 active_packages(packages)
@@ -20,9 +20,33 @@ for(col in 8:16) {
   dane_z_html2[, col] <-  as.numeric(gsub(",", ".", dane_z_html2[, col]))
 }
 dane_z_html2
-library(tidyverse)
-
 
 #zmiana nazw kolumn
 colnames(dane_z_html2)[c(1, 2,5, 6, 7, 10)]=cbind("lp", "osrodek", "metoda_badania", "uwzgl_niezdec", "termin_badania", "K15" )
 dane_z_html2
+
+
+dane_z_html2$Publikacja <- dmy(dane_z_html2$Publikacja)
+
+# Please create ggplot chart with:
+#   
+#   points
+# smooth
+# 
+# for following data:
+#   
+#   time
+# results
+# research center
+
+
+
+#JDSZ1SK-49 Project R 1 - election polls SLD -JDSZ1SK-54 
+ggplot(data = dane_z_html2, mapping = aes(x = Publikacja, y = SLD/100)) + 
+  geom_point(aes(color=osrodek))+
+  geom_smooth(span = 0.75)+
+  xlab("Data publikacji")+
+  ylab("Warości procentowe")+
+  scale_y_continuous(labels=percent_format())
+  
+              
