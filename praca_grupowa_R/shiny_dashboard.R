53bbf0a58ec0514e5619c6904a2d68e18e57a2ef
Mateusz <kieszkowski.mateusz@gmail.com>
2018-04-08 22:35:48 +0200

    dodano pozostale taski do istniejacych Marty

--- praca_grupowa_R/shiny_dashboard.R
+++ praca_grupowa_R/shiny_dashboard.R
@@ -5,31 +5,63 @@ active_packages <- function(pkg){
   sapply(pkg, require, character.only = TRUE)
   print("packages ready")
 }
-packages <- c("devtools", "plyr", "RPostgreSQL", "SnowballC", "dplyr", "shinydashboard", "scales", "wordcloud", "syuzhet", "tidyverse", "shiny", "RCurl", "XML", "lubridate", "stringr", "ggplot2", "DT", "tm")
+packages <- c("devtools", "plyr", "RPostgreSQL", "SnowballC", "dplyr", "shinydashboard", "scales", "wordcloud", "syuzhet", "tidyverse", "shiny", "RCurl", "XML", "lubridate", "stringr", "ggplot2", "DT", "tm","RColorBrewer")
 active_packages(packages)
+
+wordfunction <- function (){
+  text <- readLines("https://raw.githubusercontent.com/infoshareacademy/jdsz1-materialy-r/master/20180323_ellection_pools/parties_en.txt?token=AhmevonNS4OmfTHodbbukZrI3JbUkoibks5azmowwA%3D%3D")
+  docs <- Corpus(VectorSource(text))
+  docs <- tm_map(docs, tolower)
+  docs <- tm_map(docs, removeNumbers)
+  docs <- tm_map(docs, stemDocument)
+  docs <- tm_map(docs, removePunctuation)
+  docs <- tm_map(docs, stripWhitespace)
+  docs <- tm_map(docs, removeWords, stopwords("english"))
+  dtm <- TermDocumentMatrix(docs)
+  m <- as.matrix(dtm)
+  v <- sort(rowSums(m),decreasing=TRUE)
+  d <<- data.frame(word = names(v), freq=v)
+}
 
 ui <- dashboardPage(
   dashboardHeader(title = "Analiza Partii wyborczych"),
   dashboardSidebar(
     sidebarMenu(id = "tab",
-                menuItem("Sondaze", tabName = "wykresypartii"),
-                menuItem("Tabela Sondazy", tabName = "electpoll"),
-                menuItem("Tabela Sondazy1", tabName = "electpoll1"),
-                menuItem("Text mining", tabName ="textmining"),
-                menuItem("Text mining - sentiment", tabName = "sentim")
+                menuItem("Elect poll analysis",icon = icon("align-left"),
+                         menuSubItem("#61,Summary table",tabName = "electpoll"),
+                         menuSubItem("#62,Choose customer survey",tabName =  "electpoll1"),
+                         menuSubItem("#63.,Party Plot",tabName = "wykresypartii"),
+                         menuSubItem("#55,Geom_tile_chart", tabName = "electpoll2"),
+                         menuSubItem("#56, Parties Support", tabName = "partieschart")),
+                
+                menuItem("Text Mining", tabName = "electpoll",icon = icon("font"),
+                         menuSubItem("#65Bar frequencies", tabName = "freqbar"),
+                         menuSubItem("#66Table Frequencies", tabName ="tabbar"),
+                         menuSubItem("#67,Associations", tabName = "assocs"),
+                         menuSubItem("Text mining", tabName ="textmining"),
+                         menuItem("$64Word Frequency", tabName = "sentim"))
     )),
   dashboardBody(
     tabItems(
       tabItem(tabName = "wykresypartii", 
               uiOutput("vy"),
               plotOutput("wybranapartia")),
-      tabItem(tabName = "electpoll", 
-              h1("Wyniki wyborow"), 
-              tableOutput("epr")),
-      tabItem(tabName = "electpoll1",
-              h1("Wybierz sam jakiego osrodka sondaz chcesz zobaczyc"),
-              uiOutput("wyborosrodka"),
-              tableOutput("tabelaosrodek")),
+      tabItem(tabName = "electpoll", h1("Wyniki wyborow"), tableOutput("epr")),
+      tabItem(tabName = "electpoll1",h1("Wybierz sam jakiego osrodka sondaz chcesz zobaczyc"),
+              uiOutput("wyborosrodka"),uiOutput("wyborzleceniodawcy"),tableOutput("tabelaosrodek")),
+      tabItem(tabName = "electpoll2", plotOutput("w1")),
+      tabItem(tabName = "partieschart",plotOutput("faceitchart")),
+      tabItem(tabName = "freqbar",
+              sliderInput("freq","Minimum Frequency:", min = 1,  max = 50, value = 5),
+              sliderInput("max", "Maximum Number of Words:", min = 1,  max = 100,  value = 10),
+              plotOutput("plot")),
+      tabItem(tabName = "tabbar", 
+              numericInput("freq1",value = 1,step = 1,label = "Input your numer"),
+              tableOutput("wordtable")),
+      tabItem(tabName = "assocs", label = "Assotiations between words", 
+              textInput("textassocs", label = "Select your word to associate"),
+              numericInput("corlimit1", label = "Select your corr limit", value = 0.01, step = 0.01, max = 1),
+              verbatimTextOutput("tabassocs")),
       tabItem(tabName="textmining",
               h1("Frekwencja słów"),
               plotOutput("czest")),
@@ -118,11 +150,11 @@ server <- function(input, output){
   #################
   
   output$vy <- renderUI({ # pierwszy element wybierany z listy
-    selectInput(inputId = "Partie", "Prosze wybrac partie",choices = c("PiS","PO","K15","PARTIA_RAZEM"),selected = FALSE)
-  }
-  )
+    selectInput(inputId = "Partie", "Prosze wybrac partie",choices = c("PiS","PO","K15","PARTIA_RAZEM"),selected = "PiS")
+  })
+  
   output$wybranapartia <- renderPlot({
-    ggplot(data = sondaz1, mapping=aes(x = Publikacja, y = get(input$Partie))) +
+    ggplot(data = sondaz1, mapping=aes(x = Publikacja, y = get(input$Partie)/100)) +
       ylab(as.character(input$Partie))+
       xlab("Termin publikacji")+
       geom_point(mapping=aes(color = osrodek))+
@@ -130,19 +162,60 @@ server <- function(input, output){
       scale_y_continuous(labels = percent_format()) +
       theme(axis.text.x=element_text(angle = 45,hjust = 1))
   })
+  
   output$epr <- renderTable({  #drugi element wybierany z listy
     sondaz1
-  })
+  }) 
+  
   output$wyborosrodka <-renderUI({
     checkboxGroupInput(inputId = "osrodekx","Prosze wybrac osrodek",inline = TRUE,
                        choiceValues = unique(sondaz1$osrodek),
-                       choiceNames = unique(sondaz1$osrodek)
-    )
+                       choiceNames = unique(sondaz1$osrodek))
   })
+  
+  output$wyborzleceniodawcy <-renderUI({                     
+    checkboxGroupInput(inputId = "zleceniodawcax", "Proszę wybrać zleceniodawcę", inline = TRUE,
+                       choiceValues = unique(sondaz1$Zleceniodawca),
+                       choiceNames = unique(sondaz1$Zleceniodawca))
+  })
+  
+  
   output$tabelaosrodek <- renderTable({
-    tabelaosrodek1 <- filter(sondaz1,input$osrodekx == sondaz1$osrodek)
+    sondaz1[sondaz1$osrodek %in% input$osrodekx | sondaz1$Zleceniodawca %in% input$zleceniodawcax,]
+  })
+  
+  output$w1 <- renderPlot({
+    sondaz1 %>%
+      count(osrodek,metoda_badania)%>%
+      ggplot()+
+      geom_tile(aes(osrodek, metoda_badania,fill = n)) +
+      theme(axis.text.x=element_text(angle = 90,hjust = 1))
+  })
+  
+  output$faceitchart <- renderPlot({
+    przefilter <- filter(sondaz1, sondaz1$metoda_badania == 'CATI')
+    head(przefilter)
+    prze_trans <- gather(przefilter, "PiS":"N/Z", key = partia, value = poparcie, na.rm = FALSE)
+    head(prze_trans)
+    ggplot(data = prze_trans, aes(x = Publikacja, y = poparcie)) +
+      geom_point(mapping = aes(x = Publikacja, y = poparcie, color = osrodek)) +
+      geom_smooth() +
+      facet_wrap(~partia, scales = "free_y")
+  })
+  output$plot <- renderPlot({
+    wordfunction()
+    wordcloud(words = d$word, freq = d$freq, scale=c(4,0.5),min.freq = input$freq, max.words=input$max, 
+              random.order = TRUE,rot.per = 0.1,colors = brewer.pal(8,"Dark2"))
     
   })
+  output$wordtable <-renderTable({
+    filter(d, freq >= input$freq1)
+  })
+  
+  output$tabassocs <-renderPrint({
+    findAssocs(dtm, terms = input$textassocs, corlimit = input$corlimit1)
+  })
+  
   output$czest <- renderPlot({ # czwarty(mój) element wybierany z listy
     ggplot(data = filter(head(d,50), freq >= 1), mapping = aes(x = reorder(word, freq), y = freq)) +
       geom_bar(stat = "identity") +
