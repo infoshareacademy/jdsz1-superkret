a124f2046dfc25739bfa6e9aad0fab5bb145486d
grzegorzskipper <grzegorz.szyperek@gmail.com>
2018-05-06 22:22:39 +0200

    dodana praca domowa R4

--- r_praca_domowa/grzegorz_szyperek_praca_domowa_R4.R
+++ r_praca_domowa/grzegorz_szyperek_praca_domowa_R4.R
@@ -24,35 +24,48 @@ active_packages(packages)
 #   
 #   Deadline: 12th May
 
-#No convertiong data to a table, to be able to visulize it as baloonplot
-dt <- as.table(as.matrix(mtcars))
+#Convertiong data to a table, to be able to visulize it as baloonplot and also I am choosing for 3 pairs of variables
+dt <- as.table(as.matrix(mtcars[c(1:6),c(1,3:7)]))
 balloonplot(t(dt), main="Cars", xlab="", ylab="",
             label=T, show.margins = T)
+#as we see on a balloonplot there are two most important variables which are "disp - displacement " and "hp - horsepower" and they affect the most on rest of variables
 
-#Correspondency analysis main core
-ca_mtcars <- CA(mtcars, graph = F)
+### Correspondency analysis main core ###
+ca_mtcars <- CA(mtcars[c(1:6),c(1,3:7)], graph = F)
 print(ca_mtcars)
 
 #First method to do CA
 eig.val <- get_eigenvalue(ca_mtcars)
 eig.val
 
-#First dimension shows minimal level of explanation of variables, but if we look at the second diemnsion it shows ~95% of explanation
-# Dim.1  5.419143e-02      73.41015808                    73.41016
-# Dim.2  1.607939e-02      21.78186137                    95.19202
+#First dimension shows minimal level of explanation of variables, but if we look at the second diemnsion it shows ~99% of explanation
+# eigenvalue             variance.percent               cumulative.variance.percent
+# Dim.1 2.101360e-02     88.661801629                    88.66180
+# Dim.2 2.480799e-03     10.467132800                    99.12893
+# Dim.3 1.691167e-04      0.713547185                    99.84248
+# Dim.4 3.678873e-05      0.155221180                    99.99770
+# Dim.5 5.444572e-07      0.002297206                   100.00000
+
 
 ###   second method of CA ###
 
 #to see how dimensions and explanation of variables looks in specific dimensions lets do a visuzlization of FVIZ_SCREENPLOT
-fviz_screeplot(ca_mtcars, addlables=T, ylim=c(0, 80))
-#the biggest "ELBOW" falls on the 3rd dimension, but smaller and also important shows faster on the second dimension
+fviz_screeplot(ca_mtcars, addlables=T, ylim=c(0, 100))
+#the biggest and only important "ELBOW" falls on the 2nd dimension, rest of bends are irrevelant.
 
 
 ?fviz_contrib
 ## Lets chceck contribution of rows 
-#Lest see how the contribution of rows 
+#Lest see how the contribution of rows (specific cars on a overall contribution)
 fviz_contrib(ca_mtcars, choice = "row", axes = 1, top = 10)
+#
 fviz_contrib(ca_mtcars, choice = "row", axes = 2, top = 10)
+#Ok so no we want to see which car charcteristic has biggest inpact on a contibutory for overall 
+fviz_contrib(ca_mtcars, choice = "col", axes = 1, top = 10)
+#
+fviz_contrib(ca_mtcars, choice = "col", axes = 2, top = 10)
+#most important characteristic is horsepower and the second important characteristic is displacement
+?mtcars
 
 ### BIPLOT ###
 fviz_ca_row(ca_mtcars)
